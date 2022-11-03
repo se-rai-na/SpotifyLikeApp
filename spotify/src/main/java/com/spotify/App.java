50519ac93bab5e06b94e93c47c97224900dfcfe8
se-rai-na <111464526+se-rai-na@users.noreply.github.com>
2022-11-02 18:37:50 -0700

    Changing the ArrayList to a regular Array

--- spotify/src/main/java/com/spotify/App.java
+++ spotify/src/main/java/com/spotify/App.java
@@ -18,10 +18,13 @@ public class App {
   private static String basePath =
   "/Users/serainaburge/Documents/GitHub/SpotifyApp/wav";
   private static Scanner input = new Scanner(System.in);
+  //the amount of elements stored insude the array in the hashmap
+  private static Integer elements = 3;
   //stores song name and reference to array position with filepath, artist, year and genre
-  private static HashMap <String, ArrayList<String>> musicLibrary = new HashMap<String, ArrayList<String>>();
+  private static HashMap <String, String[]> musicLibrary = new HashMap<String, String[]>();
   //stores the play history
   private static ArrayList <String> playHistory = new ArrayList<String>();
+  
 
   // "main" makes this class a java app that can be executed
   public static void main(final String[] args) {
@@ -61,6 +64,8 @@ public class App {
 
     System.out.println("");
     System.out.print("Enter q to Quit:");
+
+    System.out.println("");
   }
 
   /*
@@ -70,6 +75,7 @@ public class App {
     switch (userInput) {
       case "h":
         System.out.println("-->Home<--");
+        home();
         break;
       case "s":
         System.out.println("-->Search by title<--");
@@ -114,13 +120,34 @@ public class App {
       String name = (String) obj.get("name");
       System.out.printf("[%d] %s\n", i + 1, name);
     }*/
+    //to only create and add to the key array once
+    //Boolean firstShow;
+    //to store keys in the hashmaps order and retrieve them
+    String[] keyMusicLibrary = new String [musicLibrary.size()];
+
+    //to account for indices in the keyMusicLibrary array
+    Integer i = 0;
 
     for(String key : musicLibrary.keySet()){
-      System.out.println(key);
-      System.out.println(musicLibrary.get(key));
+      //create array to read the song title information
+      String[] songCredits = new String [elements];
+      //read current keys object into the variable songCredits
+      songCredits = musicLibrary.get(key);
+
+      //Display number, song title and information; i+1 so the output starts from 1
+      System.out.println("["+(i+1)+"] " + key);
+      //print artist, year, genre
+      for(Integer y = 0; y < elements; y++){
+        System.out.print(songCredits[y] + ", ");
+      }
+
+      System.out.println("");
+      //Keys are saved in the array to be retrieved after receiving user input
+      keyMusicLibrary[i] = key;
+      i++;
     }
 
-    /*/ get input
+    // get input
     String userInput = input.nextLine();
     
     //get the number from the input
@@ -130,12 +157,11 @@ public class App {
     userChoice -= 1;
 
     //get the name of the song to pass to the play function as argument
-    JSONObject obj = (JSONObject) Library.get(userChoice);
-    String name = (String) obj.get("name");
+    String songTitle = keyMusicLibrary[userChoice];
 
-    System.out.println("This is the song: " + name);
+    System.out.println("This is the song: " + keyMusicLibrary[userChoice]);
     //pass name to play function to play the song
-    play(name);*/
+    play(songTitle);
 
   }
 
@@ -147,6 +173,7 @@ public class App {
     while(!correctInput){
     // get input
     String userChoice = input.nextLine();
+    //test if the song exists in the library
       if(musicLibrary.containsKey(userChoice))
       {
         correctInput = true;
@@ -154,15 +181,21 @@ public class App {
         play(userChoice);
       }
       else{
-        System.out.println("This song is not in the library. Enter another one or press q to quit.");
+        System.out.println("This song is not in the library.");
       }
     }
   }
 
   // plays an audio file
   public static void play(String songName) {
-    // add the song to the song history
-    playHistory.add(songName);
+    //add song information in array
+    String[] artistName = musicLibrary.get(songName);
+    //add song name and: artistName[0] -> add only the artist to array
+    playHistory.add(songName +", "+ artistName[0]);
+    
+
+    //Display currently played song title to user
+    System.out.println("You are listening to: " + songName);
 
     // get the filePath and open the audio file
     String fileName = songName.toLowerCase();
@@ -186,6 +219,17 @@ public class App {
       audioClip.loop(Clip.LOOP_CONTINUOUSLY);
     } catch (Exception e) {
       e.printStackTrace();
+    }
+  }
+
+  //Func: Displays the recently played song
+  //Desc: Displays the recently played song by using the global variable playHistory, 
+  //which stores all the song titles that are passed to the play function
+  //
+  public static void home(){
+    System.out.println("Your recently played songs:");
+    for(int i = 0; i < playHistory.size(); i++){
+      System.out.println("\t" + playHistory.get(i));
     }
   }
 
@@ -241,10 +285,10 @@ public class App {
       genre = (String) obj.get("genre");
 
       // Array that stores artist, year and genre
-      ArrayList <String> songInfo = new ArrayList <String>();
-      songInfo.add(artist);
-      songInfo.add(year);
-      songInfo.add(genre);
+      String[] songInfo = new String [elements];
+      songInfo[0] = artist;
+      songInfo[1] = year;
+      songInfo[2] = genre;
 
       musicLibrary.put(name, songInfo);
     }
